1f7573701a815c1091bcd4bba90634b884206f3a
neil <neil.banas@strath.ac.uk>
2017-08-04 13:50:53 +0100

    first half of a complete model rewrite that doesn't iterate in time (a,D,W so far; N and fitness not written yet)

--- coltraneModel.m
+++ coltraneModel.m
@@ -1,18 +1,24 @@
-function OUT = coltraneModel(forcing0,p);
+%function OUT = coltraneModel(forcing0,p);
 
 % out = coltraneModel(forcing0);
 % ... = coltraneModel(forcing0,p);
 %
 % time-integrates one population with a single set of traits, but variable
-% spawning date tspawn. Calculates summary metrics for both the phi and ER
-% versions of the model.
+% spawning date tspawn.
 %
-% For full model description, see Banas et al (2016) Front. Mar. Res., submitted
+% For original model description, see Banas et al, Front. Mar. Res., 2016.
 % (http://neilbanas.com/projects/coltrane)
+%
+% this version is an apporximate solution which doesn't require iteration across
+% all state variables in time--an exercise in making the model cleanly 
+% hierarchical, so that one can derive predictions about development alone (D),
+% then size and time evolution in surviving cohorts (D,W), and then mortality, 
+% survivorship, and population dynamics (D,W,N). This will also make it possible
+% for N to be density-dependent.
 
 
-if nargin<2, p = coltraneParams; end
-if nargin<1, forcing0 = coltraneForcing('simple-g'); end
+%if nargin<2, p = coltraneParams; end
+%if nargin<1, forcing0 = coltraneForcing('simple-g'); end
 
 v.tspawn = 0 : p.dt_tspawn : 365; % initial spawn date
 NC = length(v.tspawn);
@@ -29,15 +35,9 @@ forcing.P =  interp1(forcing0.t, forcing0.P,  tc);
 % initialize storage
 v.lnN = nan.*ones(N,NC); % log number of individuals per starting individual
 v.D = nan.*ones(N,NC); % developmental state, 0..1
-v.S = nan.*ones(N,NC); % structural biomass
-v.phi = nan.*ones(N,NC); % potential reserves/eggs
+v.W = nan.*ones(N,NC); % structural biomass
 v.a = nan.*ones(N,NC); % activity level
-v.R = nan.*ones(N,NC); % reserve biomass
-v.Einc = zeros(N,NC); % income egg production
-v.Ecap = zeros(N,NC); % capital egg production
 v.m = zeros(N,NC); % mortality rate
-v.Fin = zeros(N,NC); % ingestion flux
-v.Fmet = zeros(N,NC); % metabolism flux
 
 % pick a guess at adult size based on mean temperature in the forcing,
 % and pick a corresponding guess at egg size based on this. These used to be
@@ -53,105 +53,55 @@ for i=1:NC
 	f = find(t == v.tspawn(i));	
 	v.lnN(f,i) = 0;
 	v.D(f,i) = 0;
-	v.S(f,i) = v.We_theo;
-	v.phi(f,i) = 0;
-	v.R(f,i) = 0;
+	v.W(f,i) = v.We_theo;
 	v.a(f,i) = 1;
 end
 
 
-% ------------------------------------------------------------------------------
-% main loop --------------------------------------------------------------------
-small = 1e-6;
-for n=1:N-1
-	sp = forcing.t(n,:) >= v.tspawn; % the ones that have been spawned so far
-	% food-saturation and temperature-response coefficients
-	sat = forcing.P(n,:) ./ (p.Ks + forcing.P(n,:));
-	Teff = v.a(n,:) .* forcing.T(n,:) + (1-v.a(n,:)) .* forcing.Tb(n,:);
-	qd = (p.Q10d^0.1) .^ Teff;
-	qg = (p.Q10g^0.1) .^ Teff;
-	Sthm1 = max(v.We_theo, v.S(n,:)) .^ (p.theta-1);
-	% development
-	u = p.u0 .* qd .* sat .* v.a(n,:);
-	nf = v.D(n,:) < p.Df; % flag for non-feeding stage
-	u(nf) = p.u0 .* qd(nf);
-	v.D(n+1,sp) = v.D(n,sp) + u(sp).*p.dt;
-	v.D(n+1,v.D(n+1,:)>1) = 1;
-	% assimilation
-	Imax = p.I0 .* qg .* Sthm1; % max ingestion rate at T, size
-	Fin = p.r_assim .* Imax .* sat .* v.a(n,:);
-		% this is r_a I in Banas et al 2016
-	Fin(nf) = 0; % no gain in the non-feeding stages
-	% metabolism and net gain
-	Fmet = p.rm .* Imax .* (p.rb + (1-p.rb).*v.a(n,:));
-		% this is M in Banas et al 2016
-	Fmet(nf) = 0; % G = 0 for non-feeding stages
-	G = Fin - Fmet;	
-	GSdt = G .* v.S(n,:) .* p.dt;
-	% allocation of positive net gain to S and phi
-	fs = (min(v.D(n,:),1)-1) ./ (p.Ds - 1 - small);
-	fs = min(1, max(0, fs)); % fs=1 means entirely to/from S
-	GSdtpos = max(0,GSdt);
-	v.S(n+1,sp) = v.S(n,sp) + fs(sp) .* GSdtpos(sp);
-	v.phi(n+1,sp) = v.phi(n,sp) + (1-fs(sp)) .* GSdtpos(sp);
-	% negative net gain is taken from phi
-	GSdtneg = min(0,GSdt);
-	v.phi(n+1,sp) = v.phi(n+1,sp) + GSdtneg(sp);	
-	% determine diapause (instantaneous assessment; fluctuates step by step)
-	C = max(0, 1 + min(p.r_phi_max, v.phi(n,:))./v.S(n,:));
-	sat_crit = p.rm .* (1-p.rb) ./ p.r_assim + C .* p.m0./p.I0./p.r_assim;
-	dia = v.D(n,:) > p.Ddia & sat < sat_crit;
-	v.a(n+1,sp & ~dia) = 1;
-	v.a(n+1,sp & dia) = 0; % assume 0 activity during diapause
-	% mortality
-	m = p.m0 .* qg .* Sthm1 .* v.a(n,:); % mortality rate at T, size
-	v.m(n+1,:) = m;
-	v.lnN(n+1,sp) = v.lnN(n,sp) - m(sp).*p.dt;
-	
-	% now subdivide phi into R, Einc, and Ecap.
-	% start by giving R the same increment as phi
-	v.R(n+1,sp) = v.R(n,sp) + (v.phi(n+1,sp) - v.phi(n,sp));
-	% has egg production started?
-	egg = v.D(n,:) >= 1 ...
-	    & forcing.t(n,:) >= p.tegg;
-	% calculate egg production and subtract from R
-	v.Einc(n,sp & egg) = p.finc .* (1-fs(sp & egg)) .* GSdtpos(sp & egg)./p.dt;	
-	Emax = p.r_assim .* Imax .* v.S(n,:);
-	v.R(n+1,sp) = v.R(n+1,sp) - v.Einc(n,sp).*p.dt;
-	v.Ecap(n,sp & egg) = p.fcap .* min(max(0,v.R(n+1,sp & egg)./p.dt), ...
-						           Emax(sp & egg) - v.Einc(n,sp & egg));
-	v.R(n+1,sp) = v.R(n+1,sp) - v.Ecap(n,sp).*p.dt;
-	
-	% save fluxes for use in energy budget
-	v.Fin(n,:) = Fin;
-	v.Fmet(n,:) = Fmet;
-end
+% activity: a(t)  ------------------------------------
+sat = forcing.P ./ (p.Ks + forcing.P);
+C = 1;
+sat_crit = p.rm .* (1-p.rb) ./ p.r_assim + C .* p.m0./p.I0./p.r_assim;
+v.a = double(sat >= sat_crit);
+
+% temperature response factors -----------------------
+Teff = v.a .* forcing.T + (1-v.a) .* forcing.Tb;
+qd = (p.Q10d^0.1) .^ Teff;
+qg = (p.Q10g^0.1) .^ Teff;
+
+% development: D(t) ----------------------------------
+isalive = forcing.t >= repmat(v.tspawn,[N 1]); % spawned yet?
+dDdt = isalive .* p.u0 .* qd; % nonfeeding formula
+v.D = cumsum(dDdt) .* p.dt;
+isfeeding = v.D >= p.Df;
+dDdt_feeding = isalive .* p.u0 .* qd .* v.a .* sat;
+dDdt(isfeeding) = dDdt_feeding(isfeeding); % full formula 
+v.D = cumsum(dDdt) .* p.dt;
+v.D(v.D>1) = 1;
+
+% net gain and growth: W(t) --------------------------
+v.W(~isfeeding) = v.We_theo;
+% approximate curve of W(D), valid for a=1
+isgrowing = isfeeding & v.D < 1;
+satmean = sum(sat .* isgrowing) ./ sum(isgrowing);
+c = (1-p.theta) .* qg ./ qd .* p.I0 ./ p.u0 .* (p.r_assim - p.rm ./ satmean);
+v.W(isgrowing) = ((v.We_theo^(1-p.theta)) + ...
+	c(isgrowing) .* (v.D(isgrowing) - p.Df)) .^ (1/(1-p.theta));
+% cumulative diapause losses 
+dWdt_diapause = - isgrowing .* (1-v.a) .* ...
+	p.rb .* p.rm .* qg .* p.I0 .* (v.W.^p.theta);
+v.W2 = v.W + cumsum(dWdt_diapause) .* p.dt;
+% adult size
+last = v.D(1:end-1,:) < 1 & v.D(2:end,:)==1;
+v.Wa = max(v.W(1:end-1,:) .* last);
+isadult = v.D >= 1;
+Wa2 = repmat(v.Wa,[N 1]);
+v.W(isadult) = Wa2(isadult);
 
 
-v.t = forcing.t(:,1);
-v.lnN(isnan(v.lnN)) = -Inf;
-% blank everything after starvation limit is reached in the potential model
-for i=1:NC
-	f = find(v.D(:,i) > p.Df & v.phi(:,i) < -p.r_starv .* v.S(:,i));
-	if ~isempty(f)
-		v.D(f(1):end,i) = nan;
-		v.S(f(1):end,i) = nan;
-		v.phi(f(1):end,i) = nan;
-		v.lnN(f(1):end,i) = -Inf;
-		v.R(f(1):end,i) = nan;
-		v.Einc(f(1):end,i) = nan;
-		v.Ecap(f(1):end,i) = nan;
-	end
-end
-% also blank R, Einc, Ecap after starvation limit is reached in the ER model
-for i=1:NC
-	f = find(v.D(:,i) > p.Df & v.R(:,i) < -p.r_starv .* v.S(:,i));
-	if ~isempty(f)
-		v.R(f(1):end,i) = nan;
-		v.Einc(f(1):end,i) = 0;
-		v.Ecap(f(1):end,i) = 0;
-	end
-end
+% mortality and survivorship
+
+
 
 
 % organise output into structures ----------------------------------------------
@@ -161,6 +111,6 @@ OUT.var = v;
 
 
 % postprocessing ---------------------------------------------------------------
-OUT.pot = coltranePostproc_phi(v,p,forcing);
-[OUT.cohort, OUT.pop, OUT.routine] = coltranePostproc_ER(v,p,forcing);
+%OUT.pot = coltranePostproc_phi(v,p,forcing);
+%[OUT.cohort, OUT.pop, OUT.routine] = coltranePostproc_ER(v,p,forcing);
 
