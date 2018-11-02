e59b5ddddb0b3a930eada0523cdf62acfbd5de4d
neil <neil.banas@strath.ac.uk>
2018-11-02 14:09:31 +0000

    rewrite of W, R, G, E calculation as a simple time-iteration--without Wapprox and with at least one energy-budget error solved (you can no longer avoid metabolic costs by being an adult during diapause)

--- coltrane_integrate.m
+++ coltrane_integrate.m
@@ -38,6 +38,8 @@ v.a = double(~(v.yday >= repmat(v.tdia_enter,[NT 1 1]) | ...
 	% assumes that tdia_enter > tdia_exit
 t0yday = reshape(yearday(v.t0),size(v.t0));
 activeSpawning = ~(t0yday >= v.tdia_enter | t0yday <= v.tdia_exit);
+tayday = reshape(yearday(v.t0+v.dtegg),size(v.t0));
+activeSpawning2 = ~(tayday >= v.tdia_enter | tayday <= v.tdia_exit);
 								
 % temperature response factors: qd, qg  --------------------------------
 v.temp = v.a .* v.T0 + (1-v.a) .* v.Td;
@@ -55,6 +57,8 @@ isalive = v.t >= repmat(v.t0,[NT 1 1]); % spawned yet?
 if p.requireActiveSpawning
 	% eliminate (t0,s) combinations in which t0 falls during diapause
 	isalive = isalive & repmat(activeSpawning,[NT 1 1]); 
+	% likewise (t0,s) combinations in which t0+dtegg falls during diapause
+	isalive = isalive & repmat(activeSpawning2,[NT 1 1]); 
 end
 dDdt = isalive .* p.u0 .* qd; % nonfeeding formula
 v.D = cumsum(dDdt) .* dt;
@@ -93,7 +97,7 @@ is31dec = abs(v.t-first31dec) == ...
 v.D_winter = reshape(v.D(is31dec),[1 NC NS]);
 
 % flag time points at which the animal is in diapause but at a 
-% diapause-incapable stage (D < Ds in this version: Ddia has been dropped)
+% diapause-incapable stage, and mark these cases as dead
 isactive = isalive & v.a==1;
 hasbeenactive = (cumsum(isactive) >= 1);
 isfailingtodiapause = isalive & hasbeenactive & v.a==0 & v.D < p.Ddia;
@@ -101,8 +105,9 @@ hasfailedtodiapause = (cumsum(isfailingtodiapause)>1);
 v.D(hasfailedtodiapause) = nan;
 
 isalive = isalive & ~isnan(v.D);
-isineggprod = isineggprod & isalive;
+isfeeding = isfeeding & isalive;
 isgrowing = isgrowing & isalive;
+isineggprod = isineggprod & isalive;
 
 % pick a guess at adult size based on mean temperature in the forcing,
 % and pick a corresponding guess at egg size based on this.
@@ -113,62 +118,57 @@ co = (1-p.theta) .* p.GGE_nominal .* (1-p.Df) .* qoverq;
 v.Wa_theo = (co .* p.I0 ./ p.u0) .^ (1./(1-p.theta));
 v.We_theo = p.r_ea .* v.Wa_theo .^ p.exp_ea;
 
-% energy gain and growth: G(t), W(t) -------------------------------------------
-% approximate curve of W(D), valid for a=1
-% (this is used for the allometry in the calculation of net gain)
-satmean = sum(v.sat .* isgrowing) ./ sum(isgrowing);
-c = (1-p.theta) .* qg ./ qd .* p.I0 ./ p.u0 .* ...
-	(p.r_assim - p.rm ./ repmat(satmean,[NT 1 1 1]));
-c = max(c,0);
-Wapprox = zeros(size(v.D));
-We_theo_3d = repmat(v.We_theo,[NT 1 1]);
-Wapprox(isgrowing) = ((We_theo_3d(isgrowing).^(1-p.theta)) + ...
-	c(isgrowing) .* (v.D(isgrowing) - p.Df)) .^ (1/(1-p.theta));
-% net gain during the growth phase (here stored as gain * W)
-ImaxW = zeros(size(Wapprox));
-ImaxW(isgrowing) = ...
-	qg(isgrowing) .* p.I0 .* Wapprox(isgrowing) .^ p.theta;
+% energy gain, growth, egg production: G(t), W(t), R(t), E(t) ------------------
+v.G = zeros(size(v.D));
+v.W = repmat(v.We_theo,[NT 1 1]);
+v.R = zeros(size(v.D));
+v.Einc = zeros(size(v.D));
+v.E = zeros(size(v.D));
 astar = p.rb + (1-p.rb) .* v.a;
-GW = zeros(size(v.D));
-GW(isgrowing) = ImaxW(isgrowing) .* ...
-	(v.a(isgrowing) .* p.r_assim .* v.sat(isgrowing) - ...
-	p.rm .* astar(isgrowing));
-% integrate to get the correct W over development
-v.W = We_theo_3d + cumsum(GW) .* dt;
-v.W = max(0,v.W);
-% calculate the fraction of this which is reserves
-fs = ones(size(v.W));
-isstoringR = v.D >= p.Ds;
-fs(isstoringR) = (1-v.D(isstoringR)) ./ (1-p.Ds);
-fs(GW < 0) = 0;
-v.R = cumsum((1-fs).*GW) .* dt;
-% adult size Wa (= size at the moment egg prod begins)
+for n=1:NT-1
+	f = isfeeding(n,:);
+	% net gain
+	Imax_nf = qg(n,f) .* p.I0 .* v.W(n,f).^(p.theta-1);
+	I_nf = v.a(n,f) .* p.r_assim .* v.sat(n,f) .* Imax_nf;
+	M_nf = p.rm .* astar(n,f) .* Imax_nf;
+	v.G(n,f) = I_nf - M_nf;
+	GWdt = v.G(n,:) .* v.W(n,:) .* dt;
+	% allocation to growth
+	dW = GWdt;
+	dW(dW>0 & isineggprod(n,:)) = 0;
+	v.W(n+1,:) = v.W(n,:) + dW;
+	% allocation to reserves
+	fr = (v.D(n,:) - p.Ds) ./ (1 - p.Ds);
+	fr = max(0,min(1,fr));
+	fr(GWdt < 0) = 1; % all net losses come from R
+	v.R(n+1,:) = v.R(n,:) + fr .* GWdt;
+	% income egg production
+	v.Einc(n,:) = max(0,GWdt) .* isineggprod(n,:);
+	% capital egg production
+	Emax = zeros(size(GWdt));
+	Emax(f) = p.r_assim .* Imax_nf .* isineggprod(n,f);
+	Ecap = max(0, Emax - v.Einc(n,:));
+	dE = min(max(0,v.R(n,:)), Ecap.*dt);
+	v.E(n,:) = v.Einc(n,:) + dE;
+	v.W(n+1,:) = v.W(n+1,:) - dE;
+	v.R(n+1,:) = v.R(n+1,:) - dE;
+end
+% adult size Wa, Ra (= size at the moment egg prod begins)
 last = ~isineggprod(1:end-1,:,:) & isineggprod(2:end,:,:);
 v.Wa = max(v.W(1:end-1,:,:) .* last);
-Wa_3d = repmat(v.Wa,[NT 1 1]);
-v.W(isineggprod) = Wa_3d(isineggprod);
-	% at this point, set W(t) = Wa throughout the egg-production phase;
-	% once we calculate Ecap(t) below, we will subtract it
-% likewise for reserves
 v.Ra = max(v.R(1:end-1,:,:) .* last); % the max() shouldn't be necessary
-Ra_3d = repmat(v.Ra,[NT 1 1]);
-v.R(isineggprod) = Ra_3d(isineggprod);
-% calculate net gain for the egg-prod phase
-% (and for the growth phase as well: previously we calculated GW but not G)
-Imax = qg .* p.I0 .* v.W.^(p.theta-1);
-Imax(~isfeeding) = 0;
-Imax(v.W < We_theo_3d) = 0;
-I = v.a .* p.r_assim .* v.sat .* Imax;
-	% this was r_assim * I, as opposed to I, in Coltrane 1.0
-M = p.rm .* astar .* Imax;
-v.G = I - M;
+% update the estimate of We
+v.We = p.r_ea .* v.Wa .^ p.exp_ea;
+% capital fraction of egg production
+v.capfrac = sum(v.E - v.Einc) ./ sum(v.E);
 
 % check for starvation
 isstarving = isgrowing & v.R < -p.rstarv .* v.W;
 isalive = isalive & cumsum(isstarving)==0;
 isineggprod = isineggprod & isalive;
+v.E(~isalive) = 0;
 
-% mortality and survivorship: N(t) -------------------------------------
+% mortality and survivorship: N(t) ---------------------------------------------
 v.m = p.m0 .* qg .* v.a .* v.W.^(p.theta-1); % mort. rate at T, size
 v.m(~isalive) = 0;
 v.lnN = cumsum(-v.m) .* dt;
@@ -177,39 +177,13 @@ lnNa = v.lnN;
 lnNa(~isineggprod) = nan;
 v.Na = exp(max(lnNa));
 
-% egg production -------------------------------------------------------
-% income egg prod = GW once tegg is reached
-v.Einc = max(0, v.G .* v.W .* isineggprod);
-if p.requireActiveSpawning
-	v.Einc = v.Einc .* v.a;
-end
-% start by calculating what capital egg prod would be if there were
-% inexhaustible energy for it
-Emax = zeros(size(v.G));
-Emax(isineggprod) = p.r_assim .* Imax(isineggprod) .* v.W(isineggprod);
-if p.requireActiveSpawning
-	Emax = Emax .* v.a;
-end
-Ecap = max(0, Emax - v.Einc);
-% debit this from reserves until the point where it exceeds what's available
-% (may have an error up to Imax * dt)
-deltaR = cumsum(Ecap).*dt;
-toomuch = deltaR > Ra_3d;
-Ecap(toomuch) = 0;
-deltaR = cumsum(Ecap).*dt;
-v.R = v.R - deltaR;
-v.W = v.W - deltaR;
-% add them up
-v.E = (v.Einc + Ecap);
-v.capfrac = sum(Ecap) ./ sum(v.E);
-
 % contributions to fitness at each t -------------------------------------------
-v.dF1 = (v.E .* exp(v.lnN) .* dt) ./ v.We_theo;
+v.dF1 = real(v.E .* exp(v.lnN) .* dt) ./ v.We_theo;
 v.dF1(isnan(v.dF1)) = 0;
 v.tEcen = sum(v.t .* v.dF1) ./ sum(v.dF1); % center of mass of E*N
 	% this minus t0 is generation length
 		
-% clean up -------------------------------------------------------------
+% clean up ---------------------------------------------------------------------
 if strcmpi(whatToSave,'fitness only')
 	OUT = v.dF1;
 elseif strcmpi(whatToSave,'scalars only')
