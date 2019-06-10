3bf2222ea651fc6f29145f83b92e9b6705e944eb
neil <neil@ocean.washington.edu>
2019-06-10 12:54:58 +0100

    fixed a leap-year problem in yearday.m

--- utility/yearday.m
+++ utility/yearday.m
@@ -1,4 +1,9 @@
 function tc = yearday(t);
 
-[yr,mo,dy,hr,mn,sc] = datevec(t);
-tc = datenum(0.*yr,mo,dy,hr,mn,sc);
+%[yr,mo,dy,hr,mn,sc] = datevec(t);
+%tc = datenum(0.*yr,mo,dy,hr,mn,sc);
+
+[yr,~] = datevec(t);
+t0 = datenum(yr,0,0);
+tc = t - reshape(t0,size(t));
+
